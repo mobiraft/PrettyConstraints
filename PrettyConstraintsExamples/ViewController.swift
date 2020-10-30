35741a33a086511c93904b6e8c8753d9758a5b6f
hardik parmar <hardik16432@gmail.com>
2020-10-30 19:17:13 +0530

    Added Example

--- PrettyConstraintsExamples/ViewController.swift
+++ PrettyConstraintsExamples/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import PrettyConstraints
 
 class ViewController: UIViewController {
-
+    
+    let containerView:UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    let label:UILabel = {
+        let label = UILabel()
+        label.text = "Hello World"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .black
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.setUpUI()
+        
     }
-
-
+    
+    func setUpUI() {
+        
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(containerView)
+        
+        containerView.applyConstraints(.fitInSafeArea(self.view.safeAreaLayoutGuide))
+        
+        containerView.addSubview(label)
+        
+        label.applyConstraints(.center(in: self.view),
+                               .leading(to: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10, equality: .greaterThanOrEqual),
+                               .trailing(to: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10, equality: .lessThanOrEqual))
+        
+    }
+    
 }
 
