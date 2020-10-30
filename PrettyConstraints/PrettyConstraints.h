6223b8919e93f6080dc39457a5748307762a7d9c
hardik parmar <hardik16432@gmail.com>
2020-10-30 17:41:02 +0530

    Initial commit

---
+++ PrettyConstraints/PrettyConstraints.h
@@ -0,0 +1,18 @@
+//
+//  PrettyConstraints.h
+//  PrettyConstraints
+//
+//  Created by Hardik Parmar on 30/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrettyConstraints.
+FOUNDATION_EXPORT double PrettyConstraintsVersionNumber;
+
+//! Project version string for PrettyConstraints.
+FOUNDATION_EXPORT const unsigned char PrettyConstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrettyConstraints/PublicHeader.h>
+
+
---
+++ PrettyConstraintsExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrettyConstraintsExamples
+//
+//  Created by Hardik Parmar on 30/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
